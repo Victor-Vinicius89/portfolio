81bac8cd398a6959f882c83c4d5fe4a9de8ac09b
VictorFaste <jplcsgo@hotmail.com>
2019-08-18 16:38:26 -0300

    portfolio

---
+++ js/LoadingPage.js
@@ -0,0 +1,6 @@
+var i = setInterval(function() {
+  clearInterval(i);
+
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}, 6000);
